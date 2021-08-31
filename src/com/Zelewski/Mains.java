b247ad7a716063e3332392d02dbd7642775449b5
KZelewski <zelewski93@gmail.com>
2021-08-31 19:42:41 +0200

    Vovels counter

--- src/com/Zelewski/Mains.java
+++ src/com/Zelewski/Mains.java
@@ -15,5 +15,27 @@ public class Mains {
         }
         System.out.println(sum);
 
+
+        System.out.println("SECOND TASK:");
+        String myString = "aawwwwaabbccquu";
+        System.out.println(myString);
+
+        int myStringSumVovels = 0;
+
+        for(int j=0; j<myString.length(); j++){
+            if(myString.charAt(j) == 'a'||
+                    myString.charAt(j) == 'e' ||
+                    myString.charAt(j) == 'i' ||
+                    myString.charAt(j) == 'o' ||
+                    myString.charAt(j) == 'u'){
+                myStringSumVovels += 1;
+            }
+        }
+
+        System.out.println(myStringSumVovels);
+
+
+
+
     }
 }
